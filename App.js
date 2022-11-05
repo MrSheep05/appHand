bc584cf0b16643b46e7d2dd7c9e30a2e21ed068e
Filip Wachowski <fifiw2005@gmail.com>
2022-11-05 11:34:56 +0100

    Ejected android

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import CameraBox from "./components/CameraBox";
 import { StateComponent } from "./utils/state";
-
+import { BleManager } from "react-native-ble-plx";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["new NativeEventEmitter"]);
+const manager = new BleManager();
 export default function App() {
   return (
     <StateComponent>
